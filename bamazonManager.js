1cf6c99a5a21b569e3d7449382eadb1f1cc73f48
unknown <mattcmej@gmail.com>
2018-05-18 20:18:14 -0700

    Remove extraneous comments and test function, and add explanatory comments.

--- bamazonManager.js
+++ bamazonManager.js
@@ -1,9 +1,9 @@
+// Node package requirements
 var columnify = require("columnify");
 var inquirer = require("inquirer");
 var mysql = require("mysql");
 
-var randomId;
-
+// Variable to hold sql connection
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -14,12 +14,18 @@ var connection = mysql.createConnection({
     database: "bamazon"
 });
 
+// Making the sql connection
 connection.connect(function (error) {
     if (error) throw error;
+
+    // This will display once user is connected.
     console.log("Connected as id " + connection.threadId + "\n");
+
+    // This function will give the manager options.
     managerPrompt();
 });
 
+// The main function which will give the manager various options.
 function managerPrompt() {
     inquirer.prompt([
         {
@@ -43,25 +49,25 @@ function managerPrompt() {
                     break;
                 case "Add New Product":
                     addNewProduct();
-                    // testAdd();
             }
-            // console.log(response.choices);
-            // console.log(response);
+
         })
 }
 
+// Allows the manager to view the table of products
 function viewProducts() {
     connection.query("SELECT * FROM products", function (error, response) {
         if (error) throw error;
         console.log("\n" + columnify(response));
-        // connection.end();
     });
 }
 
+// Allows the manager to view any items for which the stock of items is less than 5.
 function viewLowInventory() {
     connection.query("SELECT * FROM products WHERE stock_quantity<5", function (error, response) {
         if (error) throw error;
-        // console.log(response);
+
+        // If the response is an empty array, then...
         if (response.length === 0) {
             console.log("No low inventory!");
         }
@@ -73,10 +79,14 @@ function viewLowInventory() {
     })
 }
 
+// Allows the manager to add existing items to the inventory.
 function addToInventory() {
+
+    // Shows a list of products.
     viewProducts();
 
     console.log("\n");
+
     inquirer.prompt([
         {
             type: "input",
@@ -85,23 +95,11 @@ function addToInventory() {
         }
     ])
         .then(function (response) {
-            // if (response.restockedItem)
-            // console.log(response);
+
             connection.query("SELECT * FROM products WHERE item_id=" + response.restockedItem, function (error, response2) {
                 if (error) throw error;
 
-                // console.log(response2);
-
-                // if (Object.values(response2[0]).includes(parseInt(response.restockedItem))) {
-                //     // restock(response2, response.restockedItem);
-                //     restock(response.restockedItem);
-                // }
-
-                // else {
-                //     console.log("Item not found!");
-                //     connection.end();
-                // }
-
+                // If response2 is an empty array, then...
                 if (response2.length === 0) {
                     console.log("\nItem not found!");
                     connection.end();
@@ -109,16 +107,16 @@ function addToInventory() {
 
                 else {
                     console.log("\nItem Selected: " + response2[0].product_name);
+
+                    // Restocks the item the manager has selected.
                     restock(response.restockedItem);
                 }
             })
         })
 }
 
-// function restock(item, id) {
-
-// }
-
+// Part of the addToInventory() function.
+// Restocks the item the manager has selected.
 function restock(item_id) {
     inquirer.prompt([
         {
@@ -140,6 +138,7 @@ function restock(item_id) {
         })
 }
 
+// Allows the manager to add a new product to the table.
 function addNewProduct() {
     inquirer.prompt([
         {
@@ -180,8 +179,7 @@ function addNewProduct() {
     ])
         .then(function (response) {
 
-            // console.log(response.)
-
+            // Assigns a random id to the added product, then finishes adding the item to the table.
             assignId(response.addedProduct, response.numberOfProducts, response.productDepartment, response.productPrice);
 
             
@@ -194,23 +192,27 @@ function assignId(product, quantity, department, price) {
     connection.query("SELECT item_id FROM products", function(error, response) {
         if (error) throw error;
 
-        randomId = Math.floor(Math.random() * 10000);
+        // Selects a new random id number from 1 to 9999.
+        var randomId = Math.floor(Math.random() * 10000) + 1;
+
+        // Holds the current product IDs
         var idValueArray = [];
 
+        // Adds the product IDs to the idValueArray
         for (i = 0; i < response.length; i++) {
             idValueArray.push(response[i].item_id);
         }
         
-
+        // If the random id already exists, then...
         if (idValueArray.includes(randomId)) {
-            // finishAddProduct(randomId);
-            assignId();
+
+            // Runs the function again, in effect choosing a new random id.
+            assignId(); 
         }
 
-
         else {
-            // return randomId;
-            // console.log("Quantity: " + quantity);
+
+            // Finishes adding the product to the table.
             finishAddProduct(randomId, product, quantity, department, price);
         }
        
@@ -219,43 +221,14 @@ function assignId(product, quantity, department, price) {
 }
 
 
-
+// Finishes adding the product to the table
 function finishAddProduct(randomId, product, quantity, department, price) {
     const INSERT = "INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) VALUES (" + parseInt(randomId) + ", '" + product + "', '" + department + "', " + parseInt(price).toFixed(2) + ", " + parseInt(quantity) + ")";
-    // console.log(INSERT)
     connection.query(INSERT, function (error, response2) {
         if (error) throw error;
 
-        // console.log(INSERT);
         console.log("Product Added!");
         connection.end();
     })
 }
-
-// function testAdd() {
-//     inquirer.prompt([
-//         {
-//         type: "confirm",
-//         message: "Add test value?",
-//         name: "confirmTest",
-//         default: true
-//         }
-
-//     ])
-//     .then(function(response) {
-//         if (response.confirmTest === true) {
-
-//             // var idList;
-
-//             // response (idList) will be an array of objects.
-//             // These objects have one property: item_id
-//             // Essentially an array of item ids
-//             connection.query("SELECT item_id FROM products", function(error, response) {
-//                 if (error) throw error;
-//                 // console.log(response);
-//                 // idList = response;
-//             })
-
-//             // // console.log(idList);
-            
 
